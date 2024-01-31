623668b4e7d72e61a2c5406b894b527d22c44cdd
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-01-31 21:04:02 +0630

    create Category model -mf

--- resources/views/blog-detail.blade.php
+++ resources/views/blog-detail.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Detail Page</title>
 </head>
 
 <body>
-    <h1><?= $blogs->title; ?></h1>
-    <p><?= $blogs->body; ?></p>
+    <h1>{{$blogs->title}}</h1>
+    <p>{{$blogs->body}}</p>
 </body>
 
 </html>
